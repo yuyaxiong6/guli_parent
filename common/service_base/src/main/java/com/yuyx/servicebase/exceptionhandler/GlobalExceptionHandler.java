4f112d6fb7da8bfe5f1b99da45a484ea4634f657
71100386_HN <202XTN#SsO>
2021-06-22 23:05:29 +0800

    6.22

---
+++ common/service_base/src/main/java/com/yuyx/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.yuyx.servicebase.exceptionhandler;
+
+
+import com.yuyx.commonutils.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+    /**
+     2 * 统一异常处理类
+     3 */
+ @ControllerAdvice
+ public class GlobalExceptionHandler {
+       //指定出现什么异常时执行这个方法
+        @ExceptionHandler(Exception.class)
+        @ResponseBody   //改注解是可以返回jaso
+ public R error(Exception e){
+             e.printStackTrace();
+             return R.error().message("执行了全局异常..");
+             }
+
+             //特定异常
+        @ExceptionHandler(ArithmeticException.class)
+        @ResponseBody   //该注解是可以返回jason格式
+        public R error(ArithmeticException e){
+            e.printStackTrace();
+            return R.error().message("执行了ArithmeticException异常处理..");
+        }
+ }
+
--- service/service_edu/src/main/java/com/yuyx/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/yuyx/eduservice/controller/EduTeacherController.java
@@ -1,12 +1,16 @@
 package com.yuyx.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yuyx.commonutils.R;
 import com.yuyx.eduservice.entity.EduTeacher;
+import com.yuyx.eduservice.entity.vo.TeacherQuery;
 import com.yuyx.eduservice.service.EduTeacherService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -52,5 +56,81 @@ public class EduTeacherController {
             ///结束4/////hahha
         }
     }
+
+
+    ////3.分页功能
+    @ApiOperation(value = "分页讲师列表")
+    @GetMapping("{page}/{limit}")
+    public R pageList(
+            @ApiParam(name = "page", value = "当前页码", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit){
+        Page<EduTeacher> pageParam = new Page<>(page, limit);
+        //调用方法进行分页
+        //分页的时候会把，底层封装，把分页中的数据都封装到pageparam中
+     int i=10/0;
+        eduTeacherService.page(pageParam,null);
+        List<EduTeacher> records = pageParam.getRecords();
+        long total = pageParam.getTotal();    //总记录数
+        return R.ok().data("total", total).data("rows", records); // 数据List集合
+    }
+
+    //4.条件查询带分页的方法
+    @PostMapping("PageTeacherCondition/{current}/{limit}")
+    public  R pageTeacherCondition(@PathVariable long current ,@PathVariable long limit,
+                                   @RequestBody (required = false) TeacherQuery teacherQuery )
+    {
+        //创建Page对象
+        Page <EduTeacher> pageTeacher=new Page<>(current,limit);
+        //构造条件
+        QueryWrapper<EduTeacher>wrapper=new QueryWrapper<>();
+        //动态sql
+        //判断条件值是否为空，如果不为空拼接条件
+        String name = teacherQuery.getName();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        Integer level = teacherQuery.getLevel();
+        //StringUtils 工具类，判断是否为空
+        if (!StringUtils.isEmpty(name)){
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if (!StringUtils.isEmpty(level) ) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+        //调用方法实现分页查询
+        eduTeacherService.page(pageTeacher,wrapper);
+        List<EduTeacher> records = pageTeacher.getRecords();
+        long total = pageTeacher.getTotal();
+        return R.ok().data("total", total).data("rows", records);
+
+    }
+    //5.根据讲师id进行查询
+    @ApiOperation(value = "根据ID查询讲师")
+    @GetMapping("{id}")
+    public R getById(  @ApiParam(name = "id", value = "讲师ID", required = true)
+                           @PathVariable String id){
+         EduTeacher teacher = eduTeacherService.getById(id);
+         return R.ok().data("item", teacher);
+         }
+     //6.讲师修改功能
+    @ApiOperation(value = "根据ID修改讲师")
+    @PutMapping("{id}")
+    public R updateById( @ApiParam(name = "id", value = "讲师ID", required = true)
+                             @PathVariable String id, @ApiParam(name = "teacher", value = "讲师对象", required = true)
+    @RequestBody EduTeacher teacher){
+         teacher.setId(id);
+        eduTeacherService.updateById(teacher);
+         return R.ok();
+         }
+
+
 }
 
